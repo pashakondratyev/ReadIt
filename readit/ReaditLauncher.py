8d961da5a6a810ac462b2aab61b309b69a3c3bc8
Pavel Kondratyev <paulkondratyev@hotmail.com>
2017-06-25 01:48:52 -0400

    Started writing the long_polling features

--- readit/ReaditLauncher.py
+++ readit/ReaditLauncher.py
@@ -1,13 +1,30 @@
+import calendar
 import time
 
 
-def poll():
+def report(event):
+    print(event)
+
+
+def process(reddit_result, time_after):
+    # TODO: Put code for parsing JSON here
+    # TODO: For each event check if time < created_utc
+    for event in reddit_result:
+        if time_after < event.created_utc:
+            report(event)
     return 0
 
 
+def poll():
+    return []
+
+
 def long_poll():
+    time_after = calendar.timegm(time.gmtime())
     while True:
-        poll()
+        result = poll()
+        process(result, time_after)
+        time_after = calendar.timegm(time.gmtime())
         time.sleep(.5)  # Sleeps .5 seconds between polls
 
 
