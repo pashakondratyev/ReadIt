d3e2045419d4ca2a11913934ca67ab8d3179f28b
Pavel Kondratyev <paulkondratyev@hotmail.com>
2017-06-30 00:35:56 -0400

    Fixed big bug, changed the push to include url

--- readit/NotificationDrivers.py
+++ readit/NotificationDrivers.py
@@ -4,11 +4,12 @@ import readit.config as cfg
 
 
 # Need to obfuscate API key
-def new_pushover_message(message):
+def new_pushover_message(message, url):
     user_data = {
         "token": cfg.pushover["api_key"],
         "user": cfg.pushover["user_key"],
-        "message": message
+        "message": message,
+        "url": url
     }
     response = requests.post("https://api.pushover.net/1/messages.json", data=user_data)
     print(response)
--- readit/ReaditLauncher.py
+++ readit/ReaditLauncher.py
@@ -8,32 +8,34 @@ import readit.RedditProcessor as RedditProcessor
 
 
 def process(subreddits, last_reddit_post_id):
+    newest_reddit_post_id = last_reddit_post_id
     reddit_result = RedditFetcher.get_new_posts_multi(subreddits)
     for subreddit in reddit_result:
         data = RedditPost.group_of_posts(subreddit)
         for post in data:
-            if int(post.id, 36) > last_reddit_post_id:
-                last_reddit_post_id = int(post.id, 36)
-            if RedditProcessor.check_post(post, last_reddit_post_id):
+            if RedditProcessor.check_post(post, newest_reddit_post_id):
+                print(int(post.id,36))
+                print(newest_reddit_post_id)
+                newest_reddit_post_id = int(post.id, 36)
                 report(post)
             else:
                 break
-    return last_reddit_post_id
+    return newest_reddit_post_id
 
 
 def report(post):
     print("Alert! There has been a new post of interest")
     print(post.created_utc)
     print(post.title)
-    NotificationDrivers.new_pushover_message(post.title)
+    NotificationDrivers.new_pushover_message(post.title, post.url)
 
 
 def long_poll():
     subreddits = ParseXML.get_subreddits()
     last_reddit_post_id = process(subreddits, 0)
     while True:
-        process(subreddits, last_reddit_post_id)
-        time.sleep(.5)  # Sleeps .5 seconds between polls
+        last_reddit_post_id = process(subreddits, last_reddit_post_id)
+        time.sleep(2)  # Sleeps .5 seconds between polls
 
 
 if __name__ == '__main__':
